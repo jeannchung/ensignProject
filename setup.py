3887163d85c3010c8b428de0a2c9efab01113934
jeannchung <jeannchung@gmail.com>
2021-09-25 22:52:40 -0700

    0.0.3
    - still brainstorming on how to properly tackle prompt
    - installed pandas

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 
 ## print(response.status_code) ##
 response = requests.get('https://data.cms.gov/data-api/v1/dataset/35da9162-d09f-434d-83fd-5dfe6b2c6d77/data')
@@ -10,6 +11,13 @@ data = response.text
 ## data is extracted, now need to parse data into JSON
 data_json = json.loads(data)
 
-for i in range(len(data_json)):
-    if data_json[i]['week_ending']:
-        print(data_json[i]['week_ending'])
+# for i in range(len(data_json)):
+#     if data_json[i]['provider_state']:
+#         print(data_json[i]['provider_state'])
+
+# print (data_json[0]['provider_state'])
+# print(data_json.get('provider_state', 'CA'))
+
+testState = data_json[0]['provider_state']
+print (testState)
+print (type(testState))
