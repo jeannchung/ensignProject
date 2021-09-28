788b70c98f6017a2148fe2ff0282e1dd89829051
jeannchung <jeannchung@gmail.com>
2021-09-27 22:59:14 -0700

    0.0.4
    - found some extremely relevant documentation on how to grab the specific data we're looking for with URL queries
    - managed to find one specific example: federal provider number matches 035014, had a personnel vacc % over 80 on this week_ending value
    - will need to work with the drupal documentation to take into account all the federal provider numbers but sort them by state somehow

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ensignProject
 Support developer prompt
 
+https://datatofish.com/dictionary-to-dataframe/
+https://towardsdatascience.com/15-things-you-should-know-about-dictionaries-in-python-44c55e75405c
+https://docs.google.com/spreadsheets/d/1YSTGb2bjR6cCaE6GMrCo4Hl9wW5XjTtwOVnmffhJgBo/edit#gid=0
+https://data.cms.gov/covid-19/covid-19-nursing-home-data/api-docs
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from pandas.core.frame import DataFrame
 import requests
 import json
 import pandas as pd
@@ -11,13 +12,33 @@ data = response.text
 ## data is extracted, now need to parse data into JSON
 data_json = json.loads(data)
 
+# referencing one specific dictionary for testing purposes
+# in this case, returns "015009"
+testDict = data_json[0]['federal_provider_number']
+
+
+
+# df = pd.DataFrame(testDict, columns = ['federal_provider_number'])
+
+# df.loc[df['federal_provider_number'] == '035014']
+
+
+
+
+# this URL returns the week ending value in which the provider 035014 had personnel vaccination % over 80
+# have to find all the providers in each state, get their %s, average them
+#  https://data.cms.gov/data-api/v1/dataset/35da9162-d09f-434d-83fd-5dfe6b2c6d77/data?filter[federal_provider_number]=035014&filter[percentage_of_current_healthcare_personnel_who_received_a_completed_covid_19_vaccination_at_any_time][operator]=>&filter[percentage_of_current_healthcare_personnel_who_received_a_completed_covid_19_vaccination_at_any_time ][condition][value]=80&column=week_ending
+
+
+# print (testDict)
+
+# pd.DataFrame.loc[DataFrame[testDict] == '035014']
+
+
 # for i in range(len(data_json)):
 #     if data_json[i]['provider_state']:
 #         print(data_json[i]['provider_state'])
 
-# print (data_json[0]['provider_state'])
-# print(data_json.get('provider_state', 'CA'))
 
-testState = data_json[0]['provider_state']
-print (testState)
-print (type(testState))
+# print(data_json.get('provider_state', 'CA'))
+# print (data_json[0]['federal_provider_number'])
