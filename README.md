dee628dea8ac2b0a58a75a41979a1700493a71a8
jeannchung <jeannchung@gmail.com>
2021-09-28 23:36:05 -0700

    0.0.5
    - more progress, got some code to iterate over the URLs necessary to pull the provider_state value for each provider
    - the first 80ish providers aren't having their URLs returned correctly as it seems something in the pandas or csv functionality removes numbers starting with 0, which breaks the link

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ https://datatofish.com/dictionary-to-dataframe/
 https://towardsdatascience.com/15-things-you-should-know-about-dictionaries-in-python-44c55e75405c
 https://docs.google.com/spreadsheets/d/1YSTGb2bjR6cCaE6GMrCo4Hl9wW5XjTtwOVnmffhJgBo/edit#gid=0
 https://data.cms.gov/covid-19/covid-19-nursing-home-data/api-docs
+https://data.cms.gov/api-docs
+https://www.drupal.org/docs/core-modules-and-themes/core-modules/jsonapi-module/filtering#filter-examples
+https://realpython.com/python-csv/
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+from os import stat
 from pandas.core.frame import DataFrame
 import requests
 import json
 import pandas as pd
+import csv
 
 ## print(response.status_code) ##
 response = requests.get('https://data.cms.gov/data-api/v1/dataset/35da9162-d09f-434d-83fd-5dfe6b2c6d77/data')
@@ -17,12 +19,11 @@ data_json = json.loads(data)
 testDict = data_json[0]['federal_provider_number']
 
 
-
+# # function that should turn dict into df but am using string representation of a dict and not a dict itself
+# # https://stackoverflow.com/questions/25604115/dataframe-constructor-not-properly-called-error
 # df = pd.DataFrame(testDict, columns = ['federal_provider_number'])
 
 # df.loc[df['federal_provider_number'] == '035014']
-
-
 
 
 # this URL returns the week ending value in which the provider 035014 had personnel vaccination % over 80
@@ -42,3 +43,34 @@ testDict = data_json[0]['federal_provider_number']
 
 # print(data_json.get('provider_state', 'CA'))
 # print (data_json[0]['federal_provider_number'])
+
+
+
+
+
+
+
+# # this reads the csv into a pandas df and will allow us to loop over the provider numbers
+# # loop over them and check the states for each?
+df = pd.read_csv('providers.csv')
+# print(df['provider'][0])
+
+
+# this prints the URL the we need to visit for each provider, but need to grab the value that's actually returned
+for i in range(len(df['provider'])):
+    provider = df['provider'][i]
+    if provider:
+        url = "https://data.cms.gov/data-api/v1/dataset/35da9162-d09f-434d-83fd-5dfe6b2c6d77/data?filter[federal_provider_number]={0}&column=provider_state".format(provider)
+        print (url)
+
+        # stateResponse = requests.get(url)
+        # stateData = stateResponse.text
+        # stateData_json = json.loads(stateData)
+
+        # print (stateData_json)
+
+# something in the pandas or csv logic gets rid of the "0" in front of the first 80 ish providers, rendering their returned URLs obsolete
+
+https://data.cms.gov/data-api/v1/dataset/35da9162-d09f-434d-83fd-5dfe6b2c6d77/data?filter[federal_provider_number]=035014&column=provider_state
+
+https://data.cms.gov/data-api/v1/dataset/35da9162-d09f-434d-83fd-5dfe6b2c6d77/data?filter[federal_provider_number]=35014&column=provider_state
